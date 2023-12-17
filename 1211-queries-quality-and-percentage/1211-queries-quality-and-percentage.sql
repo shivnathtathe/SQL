a4e8235e0ccf59045bcf40b9b56d6a9a4018efcb
Shivnath Tathe <76526542+shivnathtathe@users.noreply.github.com>
2023-12-17 08:06:30 +0530

    Time: 684 ms (36.95%), Space: 0B (100.00%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,9 @@
+SELECT 
+    query_name, 
+    ROUND( AVG(rating / position), 2 )
+                                AS quality,
+    ROUND( AVG( IF(RATING < 3, 1, 0) ) * 100, 2 )
+                                AS poor_query_percentage
+FROM queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
